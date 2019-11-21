9bdfb4524efee09722bd3c8fe461ead070443030
Ilmari <ilmari.koskinen1@gmail.com>
2019-11-21 14:24:35 +0200

    Fix endpoint url

--- js/registerServiceWorker.js
+++ js/registerServiceWorker.js
@@ -3,7 +3,7 @@ function registerServiceWorker() {
 
   if (serviceWorkerSupported) {
     navigator.serviceWorker
-      .register('/serviceWorker.js')
+      .register(`${document.location.href}serviceWorker.js`)
       .then(reg => console.log('service worker registered'))
       .catch(err => console.log('service worker not registered', err));
   }
