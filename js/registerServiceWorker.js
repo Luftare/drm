62f973f4b41cb052284f9c03fa3f143795de20ca
Ilmari <ilmari.koskinen1@gmail.com>
2019-11-21 15:31:37 +0200

    Update service worker url

--- js/registerServiceWorker.js
+++ js/registerServiceWorker.js
@@ -3,9 +3,8 @@ function registerServiceWorker() {
   const serviceWorkerSupported = 'serviceWorker' in navigator;
 
   if (serviceWorkerSupported) {
-    log('registering at: ' + `${document.location.href}serviceWorker.js`);
     navigator.serviceWorker
-      .register(`${document.location.href}serviceWorker.js`)
+      .register('serviceWorker.js')
       .then(reg => console.log('service worker registered'))
       .catch(err => console.log('service worker not registered', err));
   }
